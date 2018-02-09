dfc37c2809e897a982b626ca8effc5640bebcfdf
mciekurs2 <mciekurs@outlook.com>
2018-02-09 11:39:45 +0200

    added terrain

---
+++ Assets/CatchFallingPeople.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchFallingPeople : MonoBehaviour {
+
+	[SerializeField]
+	private Transform respawnTransform;
+
+	private void OnTriggerEnter(Collider collider)
+	{
+		Debug.Log ("Catched "+collider.gameObject.name);
+
+		GameObject catchedObject = collider.gameObject;
+
+		if (gameObject.tag == "Player") {
+
+			Debug.Log ("Player tag");
+			catchedObject.transform.position = respawnTransform.position;
+		}
+	}
+}
